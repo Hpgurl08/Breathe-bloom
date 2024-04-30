ba8d6da0aaeb9594e413835898416d11033afe39
Chelsea Long <98781403+Hpgurl08@users.noreply.github.com>
2024-04-30 18:19:11 -0400

    Add files via upload
    4th commit

--- server/controllers/ForumController.js
+++ server/controllers/ForumController.js
@@ -1,127 +1,103 @@
-const Forum = require("../models/Forum");
-const Comment = require("../models/Comment");
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const { LocalStorage } = require("node-localstorage");
-const localStorage = new LocalStorage("./storage");
-const User = require("../models/User");
+const Forum = require('../models/Forum');
+const Comment = require('../models/Comment');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const { LocalStorage } = require('node-localstorage');
+const localStorage = new LocalStorage('./storage');
 
-exports.getForumPage = async (req, res) => {
-  try {
-    const token = localStorage.getItem("AUTH_TOKEN");
+const token = localStorage.getItem('AUTH_TOKEN')
+console.log('local token',token)
+
+exports.getForumPage = async(req,res) =>{
+
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
-
     const posts = await Forum.find();
-    const postsWithComments = await Promise.all(
-      posts.map(async (post) => {
-        const commentsCount = await Comment.countDocuments({
-          postId: post._id,
-        });
-        const user = await User.findById(post.userId);
+    const postsWithComments = await Promise.all(posts.map(async (post) => {
+        const commentsCount = await Comment.countDocuments({ postId: post._id });
         return {
-          ...post.toObject(),
-          username: user.username,
-          commentsCount: commentsCount,
+            ...post.toObject(),
+            commentsCount: commentsCount
         };
-      })
-    );
-
+    }));
     const info = {
-      title: "Community Forum",
-      description: "Community content",
-      token: decodedToken,
-      posts: postsWithComments,
-    };
+        title: 'Community Forum',
+        description: 'Community content',
+        token: decodedToken,
+        posts:postsWithComments
+    }
+    res.render('forum', info);
+}
 
-    res.render("forum", info);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal server error");
-  }
-};
-
-exports.postForumPost = async (req, res) => {
-  try {
-    const token = localStorage.getItem("AUTH_TOKEN");
+exports.postForumPost = async(req,res,image)=>{
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
-    const { title, content, username } = req.body;
-
+    const { title,content } = req.body;
     const newPost = new Forum({
-      userId: userId,
-      username: username,
-      title: title,
-      content: content,
-      createdAt: new Date(),
-    });
+        userId:userId,
+        title: title,
+        content:content,
+        image:image,
+        createdAt:new Date()
+    })
+    try {
+        await Forum.create(newPost)
+        const posts = await Forum.find();
+        const postsWithComments = await Promise.all(posts.map(async (post) => {
+            const commentsCount = await Comment.countDocuments({ postId: post._id });
+            return {
+                ...post.toObject(),
+                commentsCount: commentsCount
+            };
+        }));
+        const info = {
+            title: 'Forum',
+            description: 'Forum content',
+            token: decodedToken,
+            posts:postsWithComments
+        }
+        res.render('forum',info)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-    await Forum.create(newPost);
-
-    const posts = await Forum.find();
-    const postsWithComments = await Promise.all(
-      posts.map(async (post) => {
-        const commentsCount = await Comment.countDocuments({
-          postId: post._id,
-        });
-        const user = await User.findById(post.userId);
-        return {
-          ...post.toObject(),
-          username: user.username,
-          commentsCount: commentsCount,
-        };
-      })
-    );
-
-    const info = {
-      title: "Forum",
-      description: "Forum content",
-      token: decodedToken,
-      posts: postsWithComments,
-    };
-
-    res.render("forum", info);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal server error");
-  }
-};
-
-exports.postComment = async (req, res) => {
-  try {
-    const token = localStorage.getItem("AUTH_TOKEN");
+exports.postComment = async(req,res)=>{
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
     const userId = decodedToken.userId;
-    const { postId, comment } = req.body;
-
-    // Find the user to get the username
-    const user = await User.findById(userId);
-
+    const { postId,comment } = req.body;
+    console.log(req.body)
     const newComment = new Comment({
-      userId: userId,
-      postId: postId,
-      username: user.username, // Save the username along with the comment
-      comment: comment,
-      createdAt: new Date(),
-    });
+        userId:userId,
+        postId: postId,
+        comment:comment,
+        createdAt:new Date()
+    })
+    try {
+        await Comment.create(newComment)
+        const posts = await Forum.find();
+        const postsWithComments = await Promise.all(posts.map(async (post) => {
+            const commentsCount = await Comment.countDocuments({ postId: post._id });
+            return {
+                ...post.toObject(),
+                commentsCount: commentsCount
+            };
+        }));
+        const info = {
+            title: 'Forum',
+            description: 'Forum content',
+            token: decodedToken,
+            posts:postsWithComments
+        }
+        res.render('forum',info)
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-    await newComment.save();
 
-    // Redirect back to the forum page
-    res.redirect("/forum");
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal server error");
-  }
-};
-
-exports.getPostComments = async (req, res) => {
-  try {
+exports.getPostComments = async(req,res)=>{
     const postID = req.query.postId;
-    const comments = await Comment.find({ postId: postID });
+    const comments = await Comment.find({postId: postID});
     res.json(comments);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal server error");
-  }
-};
+}
