09729e06b2908bdee55d9270cd5daf71fa033e19
Chelsea Long <98781403+Hpgurl08@users.noreply.github.com>
2024-05-01 14:18:43 -0400

    Add files via upload
    4th commit

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,19 +3,19 @@ const jwt = require("jsonwebtoken");
 const { LocalStorage } = require("node-localstorage");
 const localStorage = new LocalStorage("./storage");
 
-const tokenx = localStorage.getItem("AUTH_TOKEN");
+// const tokenx = localStorage.getItem("AUTH_TOKEN");
 
 const authenticateToken = (req, res, next) => {
   const tokenx = localStorage.getItem("AUTH_TOKEN");
   const token = tokenx;
   //console.log('Header token here ',token)
-  if (!token) return res.redirect("/login");
+  if (!token) return res.redirect("/");
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (error) {
-    res.redirect("/login");
+    res.redirect("/");
   }
 };
 
